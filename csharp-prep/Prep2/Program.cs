f33bc8e879a94159fdba8ca9881e2f65cab14627
Steve Swan <sswanbyui@byui.edu>
2025-09-16 10:04:16 -0600

    Code rewritten for Prep1 according to specifications.  Prep2 code was ported by hand from originial python script I wrote in CSE 110.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        float plusMinus = gradePercentage % 10;
+        string sign = "";
+        string grade = "";
+
+        if (plusMinus >= 7)
+        {
+            sign = "+";
+        }
+        if(plusMinus <= 3)
+            {
+                sign = "-";
+            }
+        if(gradePercentage >= 97)
+            {
+                sign = "";
+            }
+        if(gradePercentage <= 60)
+            {
+                sign = "";
+            }
+        if(gradePercentage >= 90)
+            {
+                grade = "A";
+            }
+        else if(gradePercentage >= 80)
+            {
+                grade = "B";
+            }
+        else if(gradePercentage >= 70)
+            {
+                grade = "C";
+            }
+        else if(gradePercentage >= 60)
+            {
+                grade = "D";
+            }
+        else
+            {
+                grade = "F";
+            }
+
+        grade = grade + sign;
+        if(gradePercentage >= 70)
+            {
+                Console.WriteLine("You passed!");
+            }
+        else
+            {
+                Console.WriteLine("You did not pass..");
+            }
+        Console.WriteLine($"Your grade is: {grade}");
+    }     
 }
